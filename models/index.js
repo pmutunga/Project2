0964be0ab83048f674032168f20bbc3912489b88
pmutunga <paskwa.mutunga@ipsafari.com>
2019-03-05 20:30:56 -0500

    travis update on index.js

--- models/index.js
+++ models/index.js
@@ -17,9 +17,7 @@ if (config.use_env_variable) {
     config.password,
     config
   );
-}
-
-fs.readdirSync(__dirname)
+}s.readdirSync(__dirname)
   .filter(function(file) {
     return (
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
