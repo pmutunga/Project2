c5c8f59cfd72d6f7b938ee291f390e8251bd7fe9
pmutunga <paskwa.mutunga@ipsafari.com>
2019-03-05 22:17:59 -0500

    update travis

--- models/schema.sql
+++ models/schema.sql
@@ -1,12 +1,4 @@
--- DROP DATABASE IF EXISTS rental_db;
--- CREATE DATABASE rental_db;
-USE rental_db;
+DROP DATABASE IF EXISTS rentaldb;
+CREATE DATABASE rentaldb;
+USE rentaldb;
 
--- DROP TABLE IF EXISTS customers;
-
---   CREATE TABLE customers
---     (        customerName VARCHAR(255) NOT NULL);
-    
--- INSERT INTO customers (customerName) VALUES ("John Doe");
-
-SELECT * FROM customers
